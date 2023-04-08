07c97f8a4e2e06d450445d08cbf03e519c5f913d
madu5 <MM218@outlook.com.br>
2023-04-08 18:40:53 -0300

    listagem automática dos arquivos finalizada

--- project-2/servidor.py
+++ project-2/servidor.py
@@ -2,68 +2,66 @@ from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 import os
 
-IP_ADRESS = "127.0.0.1"
-PORT_NUMBER = 8014
 
-def serve_folder(folder_path):
-    array_de_arquivos = []
-    for dirpath, dirnames, filenames in os.walk(folder_path):
-        print(folder_path == dirpath)
-        print(filenames)
-        for filename in filenames:
-            array_de_arquivos.append(filename)
-    return array_de_arquivos
+# Lista o conteúdo de uma pasta no html
+def list_content(html, searched_file):
+        tags = {"png": "&#128247;", "mp4": "&#127916;", "mpeg": "&#127925;", "pdf": "&#128212;"}
+
+        for i in os.listdir(searched_file):
+            if os.path.isdir(os.path.join(f'./{searched_file}', i)):
+                tag = "&#128193;"
+            else:
+                tag = tags.get(i.split(".")[-1])
+                if not tag:
+                    tag = "&#128196;"
+            endereco = f"{searched_file}/{i}"
+            html += f"<li> {tag} <a href='/{endereco}'>{i}</a> </li>"
+        
+        html += '''</ul></body></html>'''
+        return html.encode()
+
 # Trata as requisições dos clientes
-
 def handleRequest(clientSocket, clientAdress):
     clientRequest = clientSocket.recv(1024).decode() # Recebe a requisição do cliente
-    print(f"O cliente requisitou: {clientRequest}")
-
-    # print(clientRequest.split("\r\n")[0])
 
     header = clientRequest.split("\r\n")[0] # Recorta a header da solicitação
-    # print(header, len(header))
-    if len(header.split(" / ")) == 2:
-        searched_file = "./Pages/index.html"
+    if header == "GET / HTTP/1.1":
+        searched_file = "./Pages"
     else:
-        searched_file = 'data/'+header.split()[1][1:] # Descobre qual arquivo foi solicitado
+        try:
+            searched_file = header.split()[1][1:] # Descobre qual arquivo foi solicitado
+        except IndexError:
+            return # Retorna porque não requisitou nada
 
-    print(f"Header: {header}, Seached file: {searched_file}")
     extension = searched_file.split(".")[-1] # Pega a extensão do arquivo solicitado
 
-    # Caso a extensão não seja nenhuma dessas o arquivo é binário ["png", "jpg", "jpeg"...]
     is_binary = True
     if extension in ["html", "css", "js"]:
         is_binary = False
 
-    try: # Tenta abrir o arquivo
-        if is_binary:
-            file = open(searched_file, 'rb')
-            print(file)
-        else:
-            file = open(searched_file, 'r', encoding='utf-8')
+    # Se for um arquivo
+    if not (os.path.isdir(searched_file)):
+        try: # Tenta abrí-lo
+            if is_binary:
+                file = open(searched_file, 'rb')
+            else:
+                file = open(searched_file, 'r', encoding='utf-8')
 
-    except FileNotFoundError: # Caso o arquivo não seja encontrado
+        except FileNotFoundError: # Caso o arquivo não seja encontrado
+            msgHeader = 'HTTP/1.1 404 File not found \r\n' '\r\n'
+            clientSocket.sendall((msgHeader + "file not found").encode()) # Devolve a mensagem para o cliente
+            clientSocket.close() # Fecha a coneção com o cliente
+            return
 
-        # Header com status de File Not Found
-        msgHeader = 'HTTP/1.1 404 File not found \r\n' \
-                    '\r\n'
-        clientSocket.sendall((msgHeader+"file not found").encode()) # Devolve a mensagem para o cliente
-        clientSocket.close() # Fecha a coneção com o cliente
-        return # Quebra a sequência do código
+        file_content = file.read()
 
-    # Header com status de sucesso
-    msgHeader = 'HTTP/1.1 200 OK \r\n' \
-                    '\r\n'
-    
-    file_content = file.read() # Ler o conteúdo do arquivo
-    if searched_file == "./Pages/index.html":
-        print(serve_folder('./data'))
-        for i in serve_folder('./data'):
-            print(i)
-            file_content+= f"<li> <a target='_blank' href='/{i}'>{i}</a> </li>"
-        file_content += '''</ul></body></html>'''
+    # Se for uma pasta
+    if os.path.isdir(searched_file):
+        html_file = open("index.html", 'r', encoding='utf-8') # Abre o html universal para listar os arquivos
+        html_base = html_file.read() # File content recebe um html
+        file_content = list_content(html_base, searched_file)
 
+    msgHeader = 'HTTP/1.1 200 OK \r\n' '\r\n'
     # Envia o arquivo solicitado para o cliente
     if is_binary:
         clientSocket.sendall(msgHeader.encode() + file_content)
@@ -72,6 +70,11 @@ def handleRequest(clientSocket, clientAdress):
 
     clientSocket.close() # Fecha conexão com o cliente
 
+
+
+IP_ADRESS = "127.0.0.1"
+PORT_NUMBER = 8014
+
 myServerSocket = socket(AF_INET, SOCK_STREAM) # Criação do socket
 myServerSocket.bind((IP_ADRESS, PORT_NUMBER)) # Vincula nosso servidor a derterminada porta
 myServerSocket.listen() # Servidor escutando por requisições
@@ -79,7 +82,6 @@ print(f"Servidor ouvindo em {IP_ADRESS} : {PORT_NUMBER}")
 
 while True:
     clientSocket, clientAdress = myServerSocket.accept() # Aceita requisição do cliente
-    print(f"Cliente {clientAdress[0]} : {clientAdress[1]} conectado com sucesso!")
 
     # Passa o tratamento da requisição para as threads
     Thread(target=handleRequest, args=(clientSocket, clientAdress)).start()
